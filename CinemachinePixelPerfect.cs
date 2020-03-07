34aae47153cfd2a3f7cf25a8ea52c77cc70a043b
スカイラー <41244356+purrception@users.noreply.github.com>
2020-03-06 17:24:30 -0800

    Uploaded CinemachinePixelPerfect.cs

---
+++ CinemachinePixelPerfect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinemachinePixelPerfect : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    private Cinemachine.CinemachineVirtualCamera vcamScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] vcam = GameObject.FindGameObjectsWithTag("EVENT_CAMERA");
+        vcamScript = GetComponent<Cinemachine.CinemachineVirtualCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vcamScript.m_Lens.OrthographicSize = mainCamera.orthographicSize;
+    }
+}
