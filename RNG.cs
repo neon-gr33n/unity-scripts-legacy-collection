4e9079ee52626215760568f461be397f9f1eb87f
スカイラー <41244356+purrception@users.noreply.github.com>
2021-04-10 22:34:05 -0400

    first commit in forever
    RNG.cs (Currently set up as a guest account name generator)
    * Modifiable for most use cases
    * Included glyphs string can be expanded as needed.

---
+++ RNG.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class RNG : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    const string glyphs = "abcdefghijklmnopqrstuvwxyz0123456789";
+    string name;
+    string userType = "guest-";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("GenerateRandom");
+    }
+
+    IEnumerator GenerateRandom()
+    {
+        yield return new WaitForEndOfFrame();
+
+        int chars = Random.Range(6, 7);
+        for(int i = 0; i <chars; i++)
+        {
+            text.text = userType += glyphs[Random.Range(0, glyphs.Length)];
+        }
+    }
+}
