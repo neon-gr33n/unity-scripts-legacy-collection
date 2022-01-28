f026dba2690929268c7b603cc4e587b845b0b5b5
スカイラー <41244356+purrception@users.noreply.github.com>
2022-01-28 14:09:12 -0500

    Added Utility Class 'Note.cs'
    'Note.cs' as the name would imply is a simple script that you can attach to any object (or even as a required component to certain types of scripts) and type in notes of what you plan to do, or remind yourself of what that one complicated script does. It works for any project, honestly.

---
+++ Note.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    [Header("WRITE ANY ADDITIONAL INFORMATION HERE")]
+    [TextAreaAttribute]
+    [SerializeField] private string _text;
+
+}
