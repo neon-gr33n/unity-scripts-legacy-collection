dcdaf77e4781e22e7bc5deb63b83a2c6e8e574a8
スカイラー <41244356+purrception@users.noreply.github.com>
2022-02-03 12:02:51 -0500

    Update Note.cs to include a getter/setter
    Included a getter/setter! This allows for a much more broad usage of the script, in a more dynamic way.
    
    Setting the text value is very simple;
    
    Add a reference to the note component 
    (either add this to the top of your script or make a public reference to the component)
    [RequireComponent(typeof(Note))]
    
    or
    
    public Note note;
    
    then set the text value (Where you want it to be set)
    note.Text = "Hello World!"

--- Note.cs
+++ Note.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// UPDATED 
+// The text value in this script can now be accessed in other places with ease.
 public class Note : MonoBehaviour
 {
     [Header("WRITE ANY ADDITIONAL INFORMATION HERE")]
     [TextAreaAttribute]
     [SerializeField] private string _text;
+    
+    public string Text 
+    {
+        get 
+        {
+           return _text;
+        }
+        set 
+        {
+            _text = value;
+        }
+    }
 
 }
